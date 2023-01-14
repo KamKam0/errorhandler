e2261c2e615ee9e90aba3588bcfe91a81b577505
KamKam0 <86293161+KamKam0@users.noreply.github.com>
2023-01-15 00:29:48 +0100

    Annotations

--- deploy.js
+++ deploy.js
@@ -8,4 +8,5 @@ module.exports = (prog) => {
     path = path.join(symbol)
     let files = fs.readdirSync(path).filter(e => e.endsWith("js") && !["index.js", "deploy.js"].includes(e))
     files.forEach(file => process.on(file.split(".js")[0], err => require(`./${file}`)(prog, err)))
+    return true
 }
